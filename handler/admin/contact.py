ffaa3202ccae6d770a7180d0628c93b0d2ffa2ed
gavin <life_1st@live.cn>
2017-04-12 22:13:56 +0800

    add ueditor to edit blogs

---
+++ handler/admin/contact.py
@@ -0,0 +1,9 @@
+import tornado.web
+
+
+class ContactManageHandler(tornado.web.RequestHandler):
+    def __init__(self, application, request, **kwargs):
+        super(ContactManageHandler, self).__init__(application, request, **kwargs)
+
+    def get(self, *args, **kwargs):
+        self.render("admin/contact.html")
