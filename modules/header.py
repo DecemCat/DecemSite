a3cff23cca2c3f2d093c94691a590b77232c4070
gavin <life_1st@live.cn>
2017-04-14 19:26:06 +0800

    delete unused templates

--- modules/header.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'gavin'
-import tornado.web
-
-
-class HeaderModule(tornado.web.UIModule):
-
-    def render(self, heading, subheading):
-        return self.render_string('module/header.html', heading=heading, subheading=subheading)
