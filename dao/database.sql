608e547935cffb1ffb0bb6824e01b58c17848451
gavin <life_1st@live.cn>
2017-04-29 12:26:20 +0800

    add share

--- dao/database.sql
+++ dao/database.sql
@@ -4,3 +4,7 @@ db.config.insert({'key':'timeline.text', 'value':'记录九三散人的生活与
 db.config.insert({'key':'timeline.startdate', 'value':'2017,1,1'});
 db.config.insert({'key':'info.introduce', 'value':'&nbsp;&nbsp;这是一个奋斗中的程序员的个人主页，主要介绍自己在日常工作学习中记录遇到的各种问题以及编程技术讨论的地方。九三散人：软件界的小学生，主要做Java类技术开发，热衷于软件技术研究。'});
 db.config.insert({'key':'config.baidu.url', 'value':'http://data.zz.baidu.com/urls?site=www.0x12345.com&token=j4LHAhXmOzbwVjSB'})
+db.config.insert({'key':'email.smtp.server', 'value':'smtp.ym.163.com'});
+db.config.insert({'key':'email.username', 'value':'public@0x12345.com'});
+db.config.insert({'key':'email.password', 'value':'zhwkdj0283'});
+db.config.insert({'key':'post.comment.flag', 'value':'0'});
